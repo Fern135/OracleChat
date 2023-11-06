bee7258c76708d61ea6d2c3f368eaef0a710a746
dante.camblor@gmail.com <dante.camblor@gmail.com>
2023-11-06 17:23:05 -0500

    making parts for the development easier

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+// move this to separate page in config
+const (
+	// 1024 bytes per page.
+	pageSize = 1024
+
+	// max request to be made within a second
+	maxRequest = 1000
+)
+
+func main() {
+
+}
