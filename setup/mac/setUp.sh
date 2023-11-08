f3e8d61b8317dc409d024ebc9437cf5621ff928a
Fernando Camblor <35276153+Fern135@users.noreply.github.com>
2023-11-08 12:25:06 -0500

    Update setUp.sh

--- setup/mac/setUp.sh
+++ setup/mac/setUp.sh
@@ -1,3 +1,23 @@
+#!/bin/bash
+
+# Check if Golang is installed
+if ! command -v go &> /dev/null; then
+    # Golang is not installed, so install it
+    echo "Golang is not installed. Installing Golang..."
+    curl -O https://golang.org/dl/go1.17.4.linux-amd64.tar.gz
+    tar -C /usr/local -xzf go1.17.4.linux-amd64.tar.gz
+    rm go1.17.4.linux-amd64.tar.gz
+fi
+
+# Check if Node.js is installed
+if ! command -v node &> /dev/null; then
+    # Node.js is not installed, so install it
+    echo "Node.js is not installed. Installing Node.js..."
+    curl -sL https://deb.nodesource.com/setup_16.x | bash -
+    apt-get install -y nodejs
+fi
+
+echo "Golang and Node.js are installed."
 
 
 # check if go os installed
